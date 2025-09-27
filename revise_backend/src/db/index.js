b724e2716c94c58f63059322f8e875d0ab79ffc5
abubakkar-js-dev <mdabubakkars182@gmail.com>
2025-09-27 07:38:28 +0600

    updated

--- revise_backend/src/db/index.js
+++ revise_backend/src/db/index.js
@@ -3,8 +3,14 @@ const {dbName} = require('../constant.js');
 
 
 const connectMongoDb = async()=>{
+    const connectionString = process.env.MONGODB_URI;
+    // console.log("MongoDB URI:", connectionString);
+    if (!connectionString) {
+        console.error('MONGODB_URI is not defined in the environment variables.');
+        process.exit(1);
+    }
     try{
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`);
+        const connectionInstance = await mongoose.connect(`${connectionString}/${dbName}`);
         console.log('MongoDB connected successfully:', connectionInstance.connection.host);
         console.log('Database Name:', connectionInstance.connection.db.databaseName);
         return connectionInstance;
--- revise_backend/src/index.js
+++ revise_backend/src/index.js
@@ -1,10 +1,25 @@
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 const app = require('./app');
+const PORT = process.env.PORT || 5000;
+const connectMongoDb = require('./db/index.js');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+// connect to MongoDB
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+connectMongoDb()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
+    })
+    console.log('MongoDB connection established successfully');
+})
+.catch((err)=>{
+    console.error("Failed to connect to MongoDB:", err);
+    // process.exit(1);
+})
+
+
+app.get('/',(req,res)=>{
+    res.send('Welcome to the API');
+})
