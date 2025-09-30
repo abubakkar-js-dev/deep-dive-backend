a587e046062528f58c249eb4020a07a75e760f27
abubakkar-js-dev <mdabubakkars182@gmail.com>
2025-09-30 18:17:28 +0600

    updated

--- node-js-deep/API-simple/index.js
+++ node-js-deep/API-simple/index.js
@@ -1,26 +1,44 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {StatusCodes} from 'http-status-codes'
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+import buffer from "buffer";
+import fs from "fs";
+import path from "path";
+import http from "http";
 
 const app = express();
 const PORT = 3000;
 
+const STATUS = {
+  SUCCESS: "Success",
+  ERROR: "Error",
+};
+
 // middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/',(req,res)=>{
-    res.status(StatusCodes.OK)
-    res.send('Hello world, this is my first API');
+app.get("/", (req, res) => {
+  res.status(StatusCodes.OK);
+  res.send("Hello world, this is my first API");
 });
 
-app.get('/hello',(req,res)=>{
-    res.status(StatusCodes.CREATED)
-    res.send('Hello from /hello endpoint');
+app.post("/add", (req, res) => {
+  const data = [];
+  const { body } = req;
+  if (!body.name || !body.age) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send({ status: STATUS.ERROR, message: "Name and age are required" });
+  }
+  data.push(body);
+  res.status(StatusCodes.CREATED).send({ status: STATUS.SUCCESS, data });
 });
 
+app.post("/hello", (req, res) => {
+  res.status(StatusCodes.OK);
+  res.send("Hello from /hello endpoint");
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
